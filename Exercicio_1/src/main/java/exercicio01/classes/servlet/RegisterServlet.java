f510593b27944131527f48993f5550aa1ec041c4
Luiz Henrique Freitas <166095630+LuizHenriqueFreitas@users.noreply.github.com>
2025-03-28 10:38:18 -0300

    Exercicio01
    problemas com o apache, não sei se funciona, mas supostamente esta correto

---
+++ Exercicio_1/src/main/java/exercicio01/classes/servlet/RegisterServlet.java
@@ -0,0 +1,62 @@
+package exercicio01.classes.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public RegisterServlet() {
+		super();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+				req.setCharacterEncoding("UTF-8");
+				String name = req.getParameter("name");
+				String email = req.getParameter("email");
+				String emailSec = req.getParameter("email2");
+				String[] options = req.getParameterValues("options");
+				String selectedOptions = "";
+				
+				if(options != null)
+				{
+					for(String option : options)
+					{
+						selectedOptions += option + " ";
+					}
+				}
+				
+				resp.setContentType("text/html;charset=UTF-8");
+				PrintWriter writer = resp.getWriter();
+				writer.println("<!DOCTYPE html>");
+				writer.println("<html lang=\"pt-BR\">");
+				writer.println("<head>");
+				writer.println("\t<meta charset=\"UTF-8\">");
+				writer.println("\t<title>Página de retorno</title>");
+				writer.println("</head>");
+				writer.println("<body>");
+				writer.println("\t<h1>Cadastro concluido com sucesso!</h1>");
+				writer.println("\t<h2>Nome: " + name + "</h2>");
+				writer.println("\t<h2>Email: " + email + "</h2>");
+				writer.println("\t<h2>Email secundário: " + emailSec + "</h2>");
+				writer.println("\t<h2>Cursos cadastrados: " + selectedOptions + "</h2>");
+				writer.println("</body>");
+				writer.println("</html>");
+			}
+		
+		@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+		throws ServletException, IOException 
+		{
+				doGet(req, resp);
+		}
+}
